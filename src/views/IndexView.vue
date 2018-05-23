d97ceb664efe80cc8dd41d51f71720c2950d7723
Frank Wei <frank@frankwei.xyz>
2018-05-23 09:09:48 +0800

    style: change cdn of css framework

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -6,12 +6,15 @@
                         h1.title| {{$t('CryptoGirl')}} <strong class="tag is-danger">Beta</strong>
                         h2.subtitle| 虚拟货币娘化计划 ？！
                         router-link(:to="{name: 'GirlList'}"
-                        class="button is-large is-black is-rounded")| 查看在册币娘
+                        class="button is-large is-primary is-rounded")| 查看在册币娘
 </template>
 
 
 <style scoped>
 .title {
     font-size: 4rem;
+}
+.hero {
+    background: #FFF;
 }
 </style>
