cd902c5d7ba478d9c61c3fbb31b68640ad146ba9
Frank Wei <frank@frankwei.xyz>
2018-06-04 16:59:41 +0800

    fix: detect account is lock or not to avoid the bug

--- src/views/Referral.vue
+++ src/views/Referral.vue
@@ -5,13 +5,13 @@
             .hero-body
                 .container
                     h1.title| 邀请好友 Referral
-                    h2.subtitle| 推荐好友玩 Link Idol 可获得 {{'百分比'}} % 消费额的奖励!
+                    h2.subtitle| 推荐好友玩 Link Idol 可获得 {{getReferPercent}} % 消费额的奖励!
                     .field.has-addons
                         label.label|你的邀请链接:
                         .control.is-expanded
-                            input.input(type="text" :value="myRefferalLink" disabled)
+                            input.input(type="text" :value="referLink" disabled)
                         .control
-                            button.button.is-success(:data-clipboard-text="myRefferalLink")| 复制链接
+                            button.button.is-success(:data-clipboard-text="referLink")| 复制链接
                     h1.title| Share to your friends
                     .buttons
                       button.button.is-large.is-circle
@@ -32,24 +32,22 @@
                       button.button.is-large.is-circle
                         span.icon.is-medium
                           i.iconfont.icon-line
-                      button.button.is-large.is-circle
-                        span.icon.is-medium
-                          i.iconfont.icon-barcode
-                      button.button.is-large.is-circle(:data-clipboard-text="myRefferalLink")
+                      button.button.is-large.is-circle(:data-clipboard-text="referLink"
+                       @click="clipURL")
                         span.icon.is-medium
                           i.iconfont.icon-link
-                    article.message.is-dark
-                      .message-header| 你的分享邀请码
-                      .message-body
-                        img(:src="generateQrCode")
+                      img(:src="generateQrCode")
+                    //- article.message.is-dark
+                    //-   .message-header| 你的分享邀请码
+                    //-   .message-body
     #referral(v-else)
         section.hero.is-medium
             .hero-body
                 .container
-                    h1.title| 欢迎来到 Link Idol <strong class="tag is-black is-medium">星云链服</strong>
+                    h1.title| 欢迎来到 Link Idol <strong class="tag is-black is-medium">以太坊服</strong>
                     h2.subtitle| 你的好友 {{getReferrer}} 推荐你来玩 Link Idol
                     router-link(:to="toHomePageParam"
-                    class="button is-large is-primary is-circle")| 立马游玩
+                    class="button is-large is-primary")| 立马游玩
 
 
 </template>
@@ -68,15 +66,28 @@ export default {
   },
   asyncComputed: {
     async generateQrCode() {
-      const { myRefferalLink } = this;
-      const uri = await QRCode.toDataURL(myRefferalLink);
+      const { referLink } = this;
+      const uri = await QRCode.toDataURL(referLink);
       return uri;
+    },
+    async getReferPercent() {
+      /**
+             * Replace with your logic here.
+             * I think you should interact with contract to get percentage
+             * @author: Frank Wei
+             */
+      const fake = () =>
+        new Promise((res) => {
+          setTimeout(() => res('10'), 1000);
+        });
+      const result = await fake();
+      return result;
     },
   },
 
   computed: {
     ...mapState({
-      myAddress: state => state.me.address,
+      myAddress: ({ me }) => (me ? me.address : ''),
     }),
     getReferrer() {
       return this.$route.params.address;
@@ -85,15 +96,23 @@ export default {
       const referrer = this.getReferrer;
       return { name: 'HomePage', params: { referrer } };
     },
-    myRefferalLink() {
+    referLink() {
       const website = 'http://game.linkidol.pro/#';
       if (this.myAddress) {
         return `${website}/draw/${this.myAddress}`;
       }
-      return '请安装钱包插件再来';
+      return '请解锁 MetaMask 钱包再刷新访问';
     },
   },
-  methods: {},
+  methods: {
+    clipURL() {
+      this.$dialog.alert({
+        title: '复制成功',
+        message: '快去分享给你的好朋友获得返现吧',
+        type: 'is-success',
+      });
+    },
+  },
 };
 </script>
 
